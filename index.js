b1196fb1a3ea929c1b5c0e05c5c47e584f986ee0
nriascos1974 <nestorriascos74@gmail.com>
2023-04-07 20:33:35 -0500

    Actualizacion BD deploy

--- index.js
+++ index.js
@@ -17,16 +17,18 @@
 //     =====`-.____`.___ \_____/___.-`___.-'=====
 //                       `=---='
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+require('dotenv').config();
 const server = require('./src/app.js');
 const loaderDiets = require('./src/controllers/Diets/loaderDiets.js');
 const { conn } = require('./src/db.js');
+const port = process.env.PORT || 3001
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then( async () => {
   
   //*Cargo los Tipos de Dietas apenas se inicia el servidor con las provistas en la documentacion
   await loaderDiets();
-  server.listen(3001, () => {
-    console.log('%s listening at 3001'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`Listening at port ${port}`); // eslint-disable-line no-console
   });
 });
